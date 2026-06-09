c3501fab7064b878b51d0fc617f263bde94c9f07
lxh2006 <1824268148@qq.com>
2026-06-09 17:02:50 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
-    '119.91.22.7',    // 👈 你现在的电脑 IP
+    '119.91.22.7',
     'localhost',
     '127.0.0.1'
   ],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://119.91.22.7:8000/api/:path*'
+      }
+    ];
+  }
 };
 
 export default nextConfig;
